19ce62e7aab525457f33f0ae90a253c24d9ece9b
yyg <444860018@qq.com>
2019-06-20 18:24:09 +0800

    代码变更

--- Android_learn/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Android_learn/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,24 +1,28 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import java.lang.reflect.Array;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
     public static String MAIN_ACTIVITY_LOG = "activity_life_cycle";
+
+    private String[] titles = {"ListView","RecylerView"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String[] titles = {"ListView","RecylerView"};
-
         ListView mainList = findViewById(R.id.main_list_view);
-
         ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1, titles);
-
         mainList.setAdapter(arrayAdapter);
 
         Log.d(MAIN_ACTIVITY_LOG, "onCreate: ");
@@ -64,5 +68,18 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
 
         Log.d(MAIN_ACTIVITY_LOG, "onDestroy: ");
+    }
+
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        if (Array.get(titles, position).equals("ListView")) {
+            Intent intent = new Intent(this, ListViewLearn.class);
+            startActivity(intent);
+        }
+
+        if (Array.get(titles, position).equals("RecylerView")) {
+
+        }
     }
 }
