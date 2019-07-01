5734e33a8126e1fbe63012a3893a84ab09db890c
yyg <444860018@qq.com>
2019-07-01 18:37:59 +0800

    listView

--- Android_learn/app/src/main/java/com/UILearn/AnimalAdapter.kt
+++ Android_learn/app/src/main/java/com/UILearn/AnimalAdapter.kt
@@ -1,18 +1,38 @@
 package com.UILearn
 
 import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.util.SparseArray
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.TextView
+import com.example.myapplication.R
 
-class AnimalAdapter(val mContext: Context, var mData: List<AnimalModel>):BaseAdapter() {
+class AnimalAdapter<T>(val mContext: Context, val layout: Int, var mData: MutableList<T>):BaseAdapter() {
 
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+//    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+//        var holder:ViewHolder? = null
+//        var itemView:View? = convertView
+//        if (itemView == null) {
+//            itemView = LayoutInflater.from(mContext).inflate(layout, parent, false)
+//            val name = itemView.findViewById<TextView>(R.id.list_name_text)
+//            val aspeak = itemView.findViewById<TextView>(R.id.list_speak_text)
+//
+//            holder = ViewHolder(name, aspeak)
+//            itemView.tag = holder
+//        } else {
+//            holder = itemView.tag as ViewHolder
+//        }
+//        holder.nameText.text = (mData[position] as AnimalModel).name
+//        holder.speakText.text = (mData[position] as AnimalModel).aSpeak
+//
+//        return itemView as View
+//    }
 
-    override fun getItem(position: Int): Any? {
-        return null
+    override fun getItem(position: Int): T {
+            return mData[position]
     }
 
     override fun getItemId(position: Int): Long {
@@ -20,7 +40,52 @@ class AnimalAdapter(val mContext: Context, var mData: List<AnimalModel>):BaseAda
     }
 
     override fun getCount(): Int {
-        return mData.count()
+        return mData.size
+    }
+
+    fun add(data: T) {
+        mData.add(data)
+        notifyDataSetChanged()
+    }
+
+    fun remove(data: T) {
+        if (mData.contains(data)) {
+            mData.remove(data)
+            notifyDataSetChanged()
+        }
+    }
+
+    fun remove(position: Int) {
+        if (mData.size > position) {
+            mData.removeAt(position)
+            notifyDataSetChanged()
+        }
+    }
+
+    fun clear() {
+        mData.clear()
+        notifyDataSetChanged()
+    }
+
+    private class ViewHolder constructor(val context: Context, val parent: ViewGroup, val layoutRes: Int) {
+        private var mViews: SparseArray<View> = SparseArray()
+        private lateinit var item: View
+        private var position:Int = 0
+
+        companion object {
+            fun bind(context:Context, convertView: View?, parent:ViewGroup, layoutRes:Int, position: Int):ViewHolder {
+                var bviewHolder = ViewHolder(context, parent, layoutRes)
+                if (convertView !is View) {
+                    bviewHolder = ViewHolder(context, parent, layoutRes)
+                } else {
+                    bviewHolder = convertView.tag as ViewHolder
+                    bviewHolder.item = convertView
+                }
+                bviewHolder.position = position
+
+                return bviewHolder
+            }
+        }
     }
 
 }
--- Android_learn/app/src/main/java/com/UILearn/ListViewLearn.kt
+++ Android_learn/app/src/main/java/com/UILearn/ListViewLearn.kt
@@ -2,12 +2,56 @@ package com.UILearn
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.Button
+import android.widget.ListView
+import android.widget.Toast
 import com.example.myapplication.R
 
-class ListViewLearn : AppCompatActivity() {
+class ListViewLearn : AppCompatActivity(), View.OnClickListener {
+
+    var datas = mutableListOf<AnimalModel>(AnimalModel("dog","wangwang",0),
+            AnimalModel("cat","miaomiao",0),
+            AnimalModel("pig","hengheng",0))
+
+    var adapter: AnimalAdapter<AnimalModel> = AnimalAdapter(
+            this,
+            R.layout.list_view_item_learn,
+            datas
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_view_learn)
+
+        val listView = findViewById<ListView>(R.id.learn_list_view)
+        listView.adapter = adapter
+
+        listView.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
+            Toast.makeText(this@ListViewLearn,"点击了第${position}个Item",Toast.LENGTH_SHORT).show()
+        }
+
+        val addBtn:Button = findViewById(R.id.list_view_add_btn)
+        val deleteBtn:Button = findViewById(R.id.list_view_delete_btn)
+        val clearBtn:Button = findViewById(R.id.list_view_clear_btn)
+        clearBtn.setOnClickListener(this)
+
+        addBtn.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@ListViewLearn, "click add btn", Toast.LENGTH_SHORT).show()
+            }
+        })
+
+        deleteBtn.setOnClickListener{
+            adapter.remove(0)
+            Toast.makeText(this@ListViewLearn, "click delete btn", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    override fun onClick(v: View?) {
+        adapter.clear()
+        Toast.makeText(this@ListViewLearn, "click clear btn", Toast.LENGTH_SHORT).show()
     }
 }
