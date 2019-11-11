85686d0dafc725b439f4e777a2f8d5a57622fc5c
yyg <>
2019-11-11 23:14:59 +0800

    coroutines

--- Android_learn/app/src/main/java/com/coroutines_learn/CoroutinesLearnActivity.kt
+++ Android_learn/app/src/main/java/com/coroutines_learn/CoroutinesLearnActivity.kt
@@ -3,36 +3,49 @@ package com.coroutines_learn
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.example.myapplication.R
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 import kotlin.coroutines.*
 
 class CoroutinesLearnActivity : AppCompatActivity() {
 
     private val job = GlobalScope.launch {
         delay(1000L)
-        print("World!")
+        println("World!")
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_coroutines_learn)
-
+//
 //        GlobalScope.launch {
+//            println("World! ${Thread.currentThread().name}")
 //            delay(1000L)
-//            println("World!")
+//            println("World! ${Thread.currentThread().name}")
 //        }
-//        println("Hello,")
+//        println("Hello, ${Thread.currentThread().name}")
 //        runBlocking {
-//            println("block")
+//            println("block ${Thread.currentThread().name}")
 //            delay(2000L)
-//            println("block finish")
+//            println("block finish ${Thread.currentThread().name}")
 //        }
 //        println("finish")
-
+//
 //        println("Hello")
 //        job.join()
+        runBlocking {
+            try {
+                withTimeoutOrNull(1300) {
+                    job.join()
+                    repeat(1000) { i ->
+                        println("I'm sleeping $i ...")
+                        delay(500L)
+                    }
+                }
+            }catch (e: TimeoutCancellationException) {
+                e.printStackTrace()
+            }
+
+        }
+
     }
 }
