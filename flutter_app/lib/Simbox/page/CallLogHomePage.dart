1e350f41a64697cba6ee3d0db904ff1c941708f1
yyg <444860018@qq.com>
2019-11-27 18:36:53 +0800

    键盘动画

--- flutter_app/lib/Simbox/page/CallLogHomePage.dart
+++ flutter_app/lib/Simbox/page/CallLogHomePage.dart
@@ -8,75 +8,67 @@ class CallLogHomePage extends StatefulWidget {
 }
 
 class _CallLogHomePageState extends State<CallLogHomePage> with TickerProviderStateMixin {
-  AnimationController controller;
-  Animation<Offset> animation;
+  AnimationController _controller;
+  Animation<Offset> _animation;
 
   @override
   void initState() {
     super.initState();
 
-    controller =
-        AnimationController(duration: Duration(milliseconds: 2000), vsync: this);
-    controller.addStatusListener((status) {
-      if (status == AnimationStatus.completed) {
-        //AnimationStatus.completed 动画在结束时停止的状态
-        debugPrint('完成');
-//        controller.reverse();
-      } else if (status == AnimationStatus.dismissed) {
-        //AnimationStatus.dismissed 表示动画在开始时就停止的状态
-        debugPrint('消失');
-//        controller.forward();
-//        controller.dispose();
-      }
+    _controller = AnimationController(duration: Duration(milliseconds: 300), vsync: this);
+    _controller.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {}
     });
-    animation =
-        Tween(begin: Offset.zero, end: Offset(0.0, 1.0)).animate(controller);
-    //开始执行动画
-    controller.forward();
+    _animation = Tween(begin: Offset.zero, end: Offset(0.0, 1.0)).animate(_controller);
   }
 
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    var keyboardView = CustomKeyBoardView();
 
     return Scaffold(
       appBar: CardAppBar(),
-      body: Stack(
-        alignment: AlignmentDirectional.bottomCenter,
-        children: [
-          Container(
-            height: size.height,
-            width: size.width,
-            color: Colors.red,
-            child: Text("content"),
+      body: Stack(alignment: AlignmentDirectional.bottomCenter, children: [
+        Container(
+          height: size.height,
+          width: size.width,
+          color: Colors.red,
+          child: Text("content"),
+        ),
+        Positioned(
+          right: 15.0,
+          bottom: 15.0,
+          child: ClipOval(
+              child: GestureDetector(
+                onTap: () {
+                  showKeyBoard(true);
+                },
+                child: Container(
+                  width: 50,
+                  height: 50,
+                  color: Theme.of(context).primaryColor,
+                  alignment: Alignment.center,
+                  child: Icon(Icons.keyboard, color: Colors.white),
+                ),
+              )),
+        ),
+        SlideTransition(
+          position: _animation,
+          child: Container(
+            child: CustomKeyBoardView(() => showKeyBoard(false), "CallLogHomePage"),
           ),
-          SlideTransition(
-            position: animation,
-            child: Container(
-              child: keyboardView,
-            ),
-          ),
-            ]),
-
-      floatingActionButton: FloatingActionButton(
-          backgroundColor: Theme.of(context).primaryColor,
-          tooltip: 'Add',
-          child: new Icon(Icons.keyboard),
-          onPressed: () {
-            _showKeyBoard(true);
-          }
-      ),
+        ),
+      ]),
     );
   }
 
-  void _showKeyBoard(bool show) {
-
+  void showKeyBoard(bool show) {
+    show ? _controller.reverse() : _controller.forward();
   }
 
   @override
   void dispose() {
     super.dispose();
-    controller.dispose();
+    _controller.dispose();
   }
 }
--- flutter_app/lib/Simbox/page/common/CustomKeyBoardView.dart
+++ flutter_app/lib/Simbox/page/common/CustomKeyBoardView.dart
@@ -1,72 +1,170 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
 
-class CustomKeyBoardView extends StatelessWidget {
-  const CustomKeyBoardView();
+class CustomKeyBoardView extends StatefulWidget {
+  Function hiddenKeyBoard;
+  String parent;
+  CustomKeyBoardView(this.hiddenKeyBoard, this.parent);
 
+  @override
+  State<StatefulWidget> createState() {
+    return _CustomKeyBoardViewState();
+  }
+}
+
+class _CustomKeyBoardViewState extends State<CustomKeyBoardView> {
   static const int _rowCount = 3;
   static const int _columnCount = 4;
   static const double _itemAspectRatio = 2.0;
   static const double _bottomToolsViewHeight = 64;
-  double _itemWidth() => (MediaQueryData.fromWindow(window).size.width - (_rowCount - 1) * 0.5)/_rowCount;
-  double _itemHeight() => _itemWidth()/_itemAspectRatio;
-  double _keyBoardHeight() => _itemHeight() * _columnCount + (_columnCount - 1) * 0.5;
 
-  double keyBoardHeight() => _keyBoardHeight() + _bottomToolsViewHeight;
+  String _inputText = "";
+
+  double _itemWidth() =>
+      (MediaQueryData.fromWindow(window).size.width - (_rowCount - 1) * 0.5) /
+      _rowCount;
+
+  double _itemHeight() => _itemWidth() / _itemAspectRatio;
+
+  double _keyBoardHeight() =>
+      _itemHeight() * _columnCount + (_columnCount - 1) * 0.5;
+
+  double keyBoardHeight() => _keyBoardHeight() + _bottomToolsViewHeight * 2;
+  List<KeyboardDisplayModel> _keyboardDisplayDatas = _keyboardDiaplayModel();
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return _keyboardView();
+    return _keyboardView(context);
   }
 
-  Widget _keyboardView() {
+  Widget _keyboardView(BuildContext context) {
     return Container(
-        height: _keyBoardHeight() + _bottomToolsViewHeight + 0.5,
+        height: _keyBoardHeight() + _bottomToolsViewHeight * 2 + 1,
         width: MediaQueryData.fromWindow(window).size.width,
         color: Color(0xff999999),
-        child: Column(
-            children: [
-              Container (
-                color: Colors.transparent,
-                height: _keyBoardHeight() + 0.5,
-                child: GridView.builder(
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisSpacing: 0.5,
-                        mainAxisSpacing: 0.5,
-                        crossAxisCount: _rowCount,
-                        childAspectRatio: _itemAspectRatio
-                    ),
-                    itemCount: _rowCount * _columnCount,
-                    itemBuilder: (context, index) {
-                      return _getItemContainer(index);
-                    }
-                ),
+        child: Column(children: [
+          Container(
+            color: Colors.white,
+            height: _bottomToolsViewHeight,
+            alignment: Alignment.center,
+            child: TextField(
+              cursorColor: Theme.of(context).primaryColor,
+              cursorWidth: 1.0,
+              focusNode: FocusNode(canRequestFocus: false),
+              keyboardType: TextInputType.phone,
+              textAlign: TextAlign.center,
+              autocorrect: false,
+              style: TextStyle(
+                fontSize: 30,
+                color: Colors.black
               ),
-              Container(
-                color: Colors.white,
-                height: _bottomToolsViewHeight,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Icon(Icons.settings),
-
-                    Icon(Icons.settings),
-                  ],
+              controller: TextEditingController(),
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                suffixIcon: Icon(Icons.backspace, color: Colors.grey,)
+              ),
+            ),
+//            child: Text(
+//              _inputText,
+//              textAlign: TextAlign.center,
+//              maxLines: 1,
+//              style: TextStyle(fontSize: 30, color: Colors.black),
+//            ),
+          ),
+          Container(
+            color: Colors.transparent,
+            height: 0.5,
+          ),
+          Container(
+            color: Colors.transparent,
+            height: _keyBoardHeight() + 0.5,
+            child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisSpacing: 0.5,
+                    mainAxisSpacing: 0.5,
+                    crossAxisCount: _rowCount,
+                    childAspectRatio: _itemAspectRatio),
+                itemCount: _rowCount * _columnCount,
+                itemBuilder: (context, index) {
+                  return _getItemContainer(index, _keyboardDisplayDatas[index]);
+                }),
+          ),
+          Container(
+            color: Colors.white,
+            height: _bottomToolsViewHeight,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Expanded(
+                  child: FlatButton(
+                    child: Icon(
+                      Icons.settings,
+                      color: Colors.black,
+                    ),
+                    hoverColor: Colors.transparent,
+                    splashColor: Colors.transparent,
+                    onPressed: () {
+                      _gotoSettingPage();
+                    },
+                  ),
                 ),
-              )
-            ]
-        )
-    );
+                Expanded(
+                  child: Container(
+                      alignment: AlignmentDirectional.center,
+                      child: Container(
+                        height: _bottomToolsViewHeight * 0.75,
+                        width: MediaQueryData.fromWindow(window).size.width *
+                            0.33333,
+                        child: Icon(
+                          Icons.call,
+                          color: Colors.white,
+                        ),
+                        decoration: BoxDecoration(
+                            color: Theme.of(context).primaryColor,
+                            borderRadius: BorderRadius.horizontal(
+                                left: Radius.circular(
+                                    _bottomToolsViewHeight * 0.75),
+                                right: Radius.circular(
+                                    _bottomToolsViewHeight * 0.75))),
+                      )),
+                ),
+                Expanded(
+                  child: FlatButton(
+                    child: Icon(
+                      Icons.keyboard_hide,
+                      color: Colors.black,
+                    ),
+                    onPressed: () {
+                      _hiddenKeyboardView();
+                    },
+                  ),
+                )
+              ],
+            ),
+          )
+        ]));
   }
 
-  Widget _getItemContainer(int index) {
+  Widget _getItemContainer(int index, KeyboardDisplayModel model) {
     return GestureDetector(
       child: Container(
         height: _itemHeight(),
         width: _itemWidth(),
         color: Colors.white,
         alignment: Alignment.center,
-        child: Text(index.toString(), style: TextStyle(color: Colors.black, fontSize: 20)),
+        child: Column (
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text (model.title, style: TextStyle(color: Colors.black, fontSize: 22)),
+            Text(model.subTitle, style: TextStyle(color: Colors.grey, fontSize: 12))
+          ],
+        )
+
       ),
       onTap: () {
         _onTapKeyBoard(index);
@@ -74,7 +172,41 @@ class CustomKeyBoardView extends StatelessWidget {
     );
   }
 
+  static List<KeyboardDisplayModel> _keyboardDiaplayModel() {
+    List<KeyboardDisplayModel> datas = List();
+    datas.add(KeyboardDisplayModel("1", ""));
+    datas.add(KeyboardDisplayModel("2", "ABC"));
+    datas.add(KeyboardDisplayModel("3", "DEF"));
+    datas.add(KeyboardDisplayModel("4", "GHI"));
+    datas.add(KeyboardDisplayModel("5", "JKL"));
+    datas.add(KeyboardDisplayModel("6", "MNO"));
+    datas.add(KeyboardDisplayModel("7", "PQRS"));
+    datas.add(KeyboardDisplayModel("8", "TUV"));
+    datas.add(KeyboardDisplayModel("9", "WXYZ"));
+    datas.add(KeyboardDisplayModel("*", ""));
+    datas.add(KeyboardDisplayModel("0", "+"));
+    datas.add(KeyboardDisplayModel("#", ""));
+    return datas;
+  }
+
   void _onTapKeyBoard(int index) {
     print("click keyboard index = " + index.toString());
+    setState(() {
+      _inputText += _keyboardDisplayDatas[index].title;
+    });
   }
+
+  void _hiddenKeyboardView() {
+    debugPrint("_hidenKeyboardView");
+    widget.hiddenKeyBoard();
+  }
+
+  void _gotoSettingPage() {
+    debugPrint("_gotoSettingPage");
+  }
+}
+
+class KeyboardDisplayModel {
+  String title, subTitle;
+  KeyboardDisplayModel(this.title, this.subTitle);
 }
